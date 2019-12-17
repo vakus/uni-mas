28f8659c5232f0e2d47c88636a3c74e9e03b6776
v8036651 <v8036651@linux34.scm.tees.ac.uk>
2019-12-17 12:12:21 +0000

    Adds implements observer and changed when passing message in monitor

--- src/icamonitors/Monitor.java
+++ src/icamonitors/Monitor.java
@@ -6,38 +6,38 @@
 package icamonitors;
 
 import icamessages.Message;
+import java.util.Observer;
 
 /**
  *
  * @author v8036651
  */
-public abstract class Monitor 
+public abstract class Monitor implements Observer
 {
-    protected Message message;
     protected String agentName;
     /**
      * Constructor for the abstract class of monitor,
      * This is abstract because it will be extended by both GUIMonitor and CMDMonitor.
-     * @param msg
      * @param name 
      */
-    public Monitor(Message msg, String name) 
+    public Monitor(String name) 
     {
-        message = msg;
         agentName = name;
     }
     
     /**
      * Abstract method that will be overwritten that will do something based on 
      * which class it is called from when a message is received.
+     * @param message
      */
-    public abstract void ReceivedMessage ();
+    public abstract void ReceivedMessage (Message message);
     
     /**
      * Abstract method that will be overwritten that will do something based on 
      * which class it is called from when a message is sent.
+     * @param message
      */
-    public abstract void SentMessage ();
+    public abstract void SentMessage (Message message);
     
     
     
