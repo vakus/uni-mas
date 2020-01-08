b30712eafb78db6e9e5a583ced0ea9bf898aec57
v8036651 <v8036651@TU45388.windows.tees.ac.uk>
2020-01-08 15:59:57 +0000

    Message package, all java doc auto formatting and variables checked

--- src/ica/main/GuiMain.java
+++ src/ica/main/GuiMain.java
@@ -1,24 +1,34 @@
 package ica.main;
 
-
 import ica.GUI.ObserverGUI;
 import ica.metaagent.Router;
 
 /*
+ * This is the Driver class and is used to run the program in a specific way.
+ * 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 /**
+ * This is the main driver class that uses the GUI and would be given to the end
+ * user.
  *
- * @author vakus
+ * @author v8073331
  */
 public class GuiMain {
-    
+
     public static ObserverGUI gui;
     public static Router router;
-    
+
+    /**
+     *This is the main class that will run and simply loads the GUI that then 
+     * takes over from this point and all functions are called by user input 
+     * from that point onwards.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         gui = new ObserverGUI();
     }
