197ff7dd80ee5469bbf5d26c5bb3de9de0758dee
v8077971 <v8077971@TU45390.windows.tees.ac.uk>
2020-01-09 11:38:53 +0000

    Safety commit of progress in Portal Tests

--- test/ica/metaagent/PortalTest.java
+++ test/ica/metaagent/PortalTest.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,6 +12,8 @@ import ica.metaagent.Portal;
 import ica.metaagent.Router;
 import ica.messages.Message;
 import ica.messages.MessageType;
+import ica.monitors.CMDMonitor;
+import ica.monitors.Monitor;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Observer;
@@ -68,21 +70,112 @@ public class PortalTest {
     }
 
     /**
+     * Test of the addAgent method, of class Portal, where the agent that already exists.
+     */
+    @Test
+    public void testAddAgentWithExistingAgent() {
+        System.out.println("Testing the Add Agent to Portal method");
+        Portal P1 = new Portal("P1");
+        User U1 = new User("U1", P1);
+        User U2 = new User("U1", P1);
+        
+        P1.addAgent(U1.getName(), U1);
+        boolean expResult = false;
+        boolean result = true;
+        
+        try{
+            P1.addAgent(U2.getName(), U2);
+        }
+        catch(IllegalArgumentException ex){
+            result = false;
+        }
+        
+        assertTrue(expResult == result);
+    }
+    
+    /**
      * Test of the addAgent method, of class Portal, where a socket agent is added to a portal.
      * @throws IOException 
      */
     @Test
-    public void testAddSocketAgentToRouter() throws IOException{
+    public void testAddSocketAgent() throws IOException{
         System.out.println("Testing the Add Socket Agent to Router method");
-        Router R1 = new Router("Test");
-        Socket S1 = new Socket();
-        SocketAgent SA1 = new SocketAgent(R1, S1);
+        Portal p = new Portal("P1");
+        Socket s = new Socket();
+        SocketAgent sa = new SocketAgent(p, s);
         
-        R1.addAgent(SA1.getName(), SA1);
+        boolean expResult = true;
+        boolean result = true;
         
-        String expResult = "SA 1";
-        String result = R1.getMetaAgent("SA 1").getName();
-        assertEquals(expResult,result);
+        try{
+            p.addAgent("SA 1", sa);
+        }
+        catch(IllegalArgumentException ex){
+            result = false;
+        }
+        
+        assertTrue(expResult == result);
+    }
+    
+    /**
+     * Test of the addAgent method, of class Portal, where a agent is added to socket agent of a portal.
+     * @throws IOException 
+     */
+    @Test
+    public void testAddAgentToSocketAgent() throws IOException{
+        System.out.println("Testing the Add Socket Agent to Router method");
+        Portal p = new Portal("P1");
+        Socket s = new Socket();
+        User u = new User("SA 1", p);
+        SocketAgent sa = new SocketAgent(p, s);
+        boolean expResult = false;
+        boolean result = true;
+        
+        try{
+            p.addAgent("SA 1", sa);
+        }
+        catch(IllegalArgumentException ex){
+            result = false;
+        }
+        
+        try{
+            p.addAgent("SA 1", u);
+        }
+        catch(IllegalArgumentException ex){
+            result = false;
+        }
+        
+        assertTrue(expResult == result);
+    }
+    
+    /**
+     * Test of the addAgent method, of class Portal, where two socket agents are added to a portal.
+     * @throws IOException 
+     */
+    @Test
+    public void testAddTwoSocketAgents() throws IOException{
+        System.out.println("Testing the Add Socket Agent to Router method");
+        Portal p = new Portal("P1");
+        Socket s = new Socket();
+        SocketAgent sa = new SocketAgent(p, s);
+        SocketAgent sa2 = new SocketAgent(p, s);
+        boolean expResult = false;
+        boolean result = true;
+        
+        try{
+            p.addAgent("SA 1", sa);
+        }
+        catch(IllegalArgumentException ex){
+            result = false;
+        }
+        
+        try{
+            p.addAgent("SA 2", sa2);
+        }
+        catch(IllegalArgumentException ex){
+            result = false;
+        }
+        assertTrue(expResult == result);
     }
     
     /**
@@ -102,7 +195,6 @@ public class PortalTest {
         
         assertEquals(sExpResult, sResult);
     }
-
 
     /**
      * Test of messageHandler method, of class Portal.
@@ -139,8 +231,8 @@ public class PortalTest {
      * Test of the isNameAllowed method, of class Portal, with an invalid name
      */
     @Test
-    public void testIsNameAllowedWithAnInvalidName(){
-        System.out.println("Testing the is name allowed method");
+    public void testIsNameAllowedWhereNameExists(){
+        System.out.println("Testing the is name allowed method, where the name exists");
         Portal instance = new Portal("Test Portal");
         User agent1 = new User("Agent", instance);
         User agent2 = new User("Agent", instance);
@@ -152,6 +244,24 @@ public class PortalTest {
         assertTrue(result == expResult);
     }
 
+    /**
+     * Test of the isNameAllowed method, of class Portal, with an invalid name
+     */
+    @Test
+    public void testIsNameAllowedWhereNameIsInvalid(){
+        System.out.println("Testing the is name allowed method, where the name is invalid");
+        
+        Portal instance = new Portal("Test Portal");
+        User agent1 = new User("Agent", instance);
+        
+        instance.addAgent("Agent", agent1);
+        
+        boolean expResult = false;
+        boolean result = instance.isNameAllowed("global");
+        
+        assertTrue(result == expResult);
+    }
+    
     /**
      * Test of isMessageOriginCorrect method, of class Portal.
      */
@@ -168,5 +278,53 @@ public class PortalTest {
         boolean result = instance.isMessageOriginCorrect(agent, msg);
         assertEquals(expResult, result);
     }
+
+    /**
+     * Test of addObserver method, of class Portal.
+     */
+    @Test
+    public void testAddObserver() {
+        System.out.println("Testing the add observer method");
+        CMDMonitor m = new CMDMonitor("M1");
+        Portal instance = new Portal("P1");
+        instance.addObserver(m);
+    }
+
+    /**
+     * Test of forwardGlobal method, of class Portal.
+     */
+    @Test
+    public void testForwardGlobal() {
+        System.out.println("forwardGlobal");
+        MetaAgent source = null;
+        Message msg = null;
+        Portal instance = null;
+        instance.forwardGlobal(source, msg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of shutdown method, of class Portal.
+     */
+    @Test
+    public void testShutdown() {
+        System.out.println("Testing the shutdown method");
+        Portal instance = new Portal("P1");
+        instance.shutdown();
+    }
     
+    /**
+     * Test of shutdown method, of class Portal, with socket agents
+     */
+    @Test
+    public void testShutdownWithSocketAgent() {
+        System.out.println("Testing the shutdown method with a socket agent on the portal");
+        Portal instance = new Portal("P1");
+        Socket s = new Socket();
+        SocketAgent sa = new SocketAgent(instance, s);
+        
+        instance.addAgent("SA1",sa);
+        instance.shutdown();
+    }
 }
