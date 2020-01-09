4f038d3c64b68512a26bd7bd41571c9872bcf4e7
vakus <sysojewosinskidominik@gmail.com>
2020-01-09 13:39:47 +0000

    updated Javadoc Removed unnecessary dimensions from ObserverInterface and UserInterace

--- src/ica/main/GuiMain.java
+++ src/ica/main/GuiMain.java
@@ -1,15 +1,10 @@
+/**
+ * This is the Driver class and is used to run the program in a specific way.
+ */
 package ica.main;
 
 import ica.GUI.ObserverGUI;
 import ica.metaagent.Router;
-
-/*
- * This is the Driver class and is used to run the program in a specific way.
- * 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  * This is the main driver class that uses the GUI and would be given to the end
@@ -23,11 +18,11 @@ public class GuiMain {
     public static Router router;
 
     /**
-     *This is the main class that will run and simply loads the GUI that then 
-     * takes over from this point and all functions are called by user input 
+     * This is the main class that will run and simply loads the GUI that then
+     * takes over from this point and all functions are called by user input
      * from that point onwards.
-     * 
-     * @param args
+     *
+     * @param args arguments passed by the console
      */
     public static void main(String[] args) {
         gui = new ObserverGUI();
